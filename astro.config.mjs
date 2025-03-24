7ed6d0cfc4c67e6573cbd05a9d7d48ae264c3ae6
Miguel Oswaldo Yepez Gonzalez <61588404+ingmiguelyepezcivil@users.noreply.github.com>
2025-03-24 15:39:18 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://miguelyepez.github.io',
     integrations: [
         mdx(),
         sitemap(),
